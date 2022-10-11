4d95da661d87e686cf721471d46c373af118e0a8
Dario Aguayo <pupaguayo@gmail.com>
2022-10-11 00:48:57 -0300

    feat-bonus-task2: added test to checked correct render pages

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render } from "@testing-library/react";
+import App from "./App";
+import { Home } from "./pages/home";
+import { CharacterDetails } from "./pages/character-details";
+import { NotFound } from "./pages/not-found/index";
+import { BrowserRouter } from "react-router-dom";
+describe("Star Wars APP", () => {
+  it("render 404 Page", () => {
+    render(<NotFound />, { wrapper: BrowserRouter });
+  });
+  it("render home page", () => {
+    render(<Home />, { wrapper: BrowserRouter });
+  });
+  it("render details page", () => {
+    render(<CharacterDetails />, { wrapper: BrowserRouter });
+  });
 });
